9ddf99f4a990570a8bbc6b66e8008551348a7b4d
dun <DundeeDevelops@gmail.com>
2023-08-20 21:23:49 -0500

    Added icon and title

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ function App() {
 
   return (
     <>
-      
-    </>
+      <h1>Social Mirror</h1>
+      <p>Create and curate your ideal online professional presence with a profile preview of major social media webistes.
+</p>
+    </> 
   )
 }
 
